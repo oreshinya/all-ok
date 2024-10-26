ea8c994acdedcee61758cc66f106c8e2514feec3
Shinya Takahashi <s.takahashi313@gmail.com>
2024-10-26 19:52:10 +0900

    To non-empty

--- src/result.ts
+++ src/result.ts
@@ -2,8 +2,11 @@ export type ErrorInfo<TLabel> = { label: TLabel; message: string };
 
 export type Result =
   | { ok: true }
-  | { ok: false; errors: Array<ErrorInfo<string>> };
+  | {
+      ok: false;
+      errors: [ErrorInfo<string>, ...ErrorInfo<string>[]];
+    };
 
 export function toResult(errors: Array<ErrorInfo<string>>): Result {
-  return errors.length <= 0 ? { ok: true } : { ok: false, errors };
+  return (errors.length <= 0 ? { ok: true } : { ok: false, errors }) as Result;
 }
