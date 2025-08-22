306e74f0585b44b8b0ac65f1fbe21d523eee6b28
Shinya Takahashi <s.takahashi313@gmail.com>
2025-08-23 00:20:46 +0900

    Update biome

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,24 @@
-export type { CheckSync } from "./check-sync";
-export { checkSync } from "./check-sync";
-
 export type { CheckAsync } from "./check-async";
 export { checkAsync } from "./check-async";
-
-export type {
-  Options,
-  MapSync,
-  MapAsync,
-  ValidationSync,
-  ValidationAsync,
-} from "./validation";
-export {
-  mapSync,
-  mapAsync,
-} from "./validation";
-
-export {
-  runSync,
-  runSyncWithContext,
-} from "./run-sync";
-
+export type { CheckSync } from "./check-sync";
+export { checkSync } from "./check-sync";
+export type { Brand, BrandKey, BrandName, ErrorInfo, Result } from "./result";
 export {
   runAsync,
   runAsyncWithContext,
 } from "./run-async";
-
-export type { Brand, BrandKey, BrandName, ErrorInfo, Result } from "./result";
+export {
+  runSync,
+  runSyncWithContext,
+} from "./run-sync";
+export type {
+  MapAsync,
+  MapSync,
+  Options,
+  ValidationAsync,
+  ValidationSync,
+} from "./validation";
+export {
+  mapAsync,
+  mapSync,
+} from "./validation";
